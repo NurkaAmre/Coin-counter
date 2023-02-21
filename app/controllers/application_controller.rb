61eb3e46133c9bdf1d570b9a87cb4ffea2bf85b7
NurkaAmre <nurkerey30@gmail.com>
2023-02-21 20:00:58 +0600

    Update schema

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,10 +7,10 @@ class ApplicationController < ActionController::Base
 
   def update_allowed_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit(:name, :email, :password, :avatar, :password_confirmation)
+      u.permit(:name, :email, :password, :icon, :password_confirmation)
     end
     devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:name, :email, :avatar, :password, :current_password)
+      u.permit(:name, :email, :icon, :password, :current_password)
     end
   end
 
