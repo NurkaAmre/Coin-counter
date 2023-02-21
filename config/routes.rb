597b6d99899618b8b09f70136231248a053300fd
NurkaAmre <nurkerey30@gmail.com>
2023-02-21 12:49:17 +0600

    Add bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions',
-    registrations: 'users/registrations',
-    passwords: 'users/passwords'
-  }
-
-  resources :entities, only: [:index]
-  resources :groups, only: [:index]
-  resources :users, only: [:index]
-  
-  devise_scope :user do
-    authenticated :user do
-      root to: 'groups#index', as: :authenticated_root
+  get '/home', to: 'home#index', as: 'home'
+  get 'entities/new'
+  get 'entities/destroy'
+  get 'entities/edit'
+  get 'entities/update'
+  get 'entities/index'
+  devise_for :users, sign_out_via: [:get, :post] 
+  # controllers: { registrations: "users/registrations" }
+  resources :users, only: [:index, :show, :destroy] do
+    resources :groups do
+      resources :entities
     end
   end
-
-    unauthenticated do
-      root to: 'home#index'
-    end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  # Defines the root path route ("/")
+  root 'groups#index'
   end
