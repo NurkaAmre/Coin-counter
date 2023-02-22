0175e550f403c9cd639f692cea21aa4459aefa8b
NurkaAmre <nurkerey30@gmail.com>
2023-02-22 21:52:10 +0600

    Implemented entity views

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,60 +1,23 @@
 class EntitiesController < ApplicationController
-  before_action :authenticate_user!
-  before_action :set_group
-  before_action :set_entity, only: %i[show edit update destroy]
-
-  def index
-    @header = 'Entities'
-    @group = Group.find(params[:id])
-    @entities = @group.entities.where(author: current_user).order(created_at: :desc)
-  end
-
   def new
-    @entity = @group.entities.build
+    @entity = Entity.new
+    @group = Group.find(params[:group_id])
   end
 
   def create
-    @entity = @group.entities.build(entity_params)
+    @group = Group.find(params[:group_id])
+      @entity = Entity.new(entity_params)
+      @entity.user = current_user
+      @entity.group = @group
 
-    respond_to do |format|
       if @entity.save
-        format.html do
-          redirect_to user_group_entities_path(current_user, @group), notice: 'Entity was successfully created.'
-        end
-        format.json { render :show, status: :created, location: @entity }
-      else
-        format.html { render :new }
-        format.json { render json: @entity.errors, status: :unprocessable_entity }
+        redirect_to user_groups_path(current_user)
       end
-    end
-  end
-
-  def edit; end
-
-  def update
-    if @entity.update(entity_params)
-      redirect_to user_group_entities_path(@entity.group), notice: 'Entity was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @entity.destroy
-    redirect_to user_group_entities_path(@entity.group), notice: 'Entity was successfully destroyed.'
   end
 
   private
 
-  def set_group
-    @group = Group.find(params[:id])
-  end
-
-  def set_entity
-    @entity = Entity.find(params[:id])
-  end
-
   def entity_params
-    params.require(:entity).permit(:name, :amount, :author_id)
+    params.require(:entity).permit(:name, :amount)
   end
 end
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -8,7 +8,9 @@ class GroupsController < ApplicationController
   end
 
   # GET /groups/1 or /groups/1.json
-  def show; end
+  def show
+    @group = Group.find(params[:id])
+  end
 
   # GET /groups/new
   def new
