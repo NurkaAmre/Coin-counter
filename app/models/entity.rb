b64e262e7c65ed26738021871c4cbe97d0d41ab5
NurkaAmre <nurkerey30@gmail.com>
2023-02-22 15:22:48 +0600

    Implement model entity

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,6 +1,7 @@
 class Transaction < ApplicationRecord
   belongs_to :users, class_name: 'User', foreign_key: :user_id
   belongs_to :groups
+  
   validates :name, :group, presence: true
   validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
